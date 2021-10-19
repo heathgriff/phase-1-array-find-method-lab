a5f1a3d8cf5793c707aba3db9bbee9fd4957a5f0
Heath Griffith <heathlgriffith@gmail.com>
2021-10-18 17:19:14 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+// const superbowlWin = record.find(function(result, year){
+//     if (result === W) {
+//         return year;
+//     } else return undefined;
+// })
+
+function superbowlWin(record) {
+    let winningYear = record.find(obj => obj.result === 'W')
+    return (winningYear ? winningYear.year : undefined )
+}
 
